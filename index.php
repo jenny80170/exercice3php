07a84c83cbc2e8a11f0822d53aec195e7f2a06fe
jennifer <jennifer.malleret@outlook.fr>
2019-07-25 11:36:56 +0200

    exercice 3 php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 3</title>
+</head>
+<body>
+  <h1>Variable number et calcul</h1>
+    <?php
+    $number = 140;
+      echo '$number égale 140 :' .$number. '<br>';
+    $number += 30;
+      echo 'on ajoute 30 à $number :' .$number. '<br>';
+    $number /= 2;
+      echo 'on divise $number par 2 :' .$number. '<br>';
+      echo 'Résultat des opérations :' .$number;
+      ?>
+    </body>
+    </html>
